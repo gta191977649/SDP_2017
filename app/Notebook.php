f32bf98d5c1c8beebdd4fe2e62054234250a1fae
Le Cai <episodes27@gmail.com>
2017-09-18 12:48:37 +1000

    add the completed history function
    now the deleted notebooks can be view and see the history

--- app/Notebook.php
+++ app/Notebook.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 
@@ -11,10 +12,15 @@ class Notebook extends Model
     //
 
     protected $fillable=['name']; //POST Filll
-   
+    use SoftDeletes;
     //define releation ship
     public function notes()
     {
-        return $this->hasMany("App\Note");
+        return $this->hasMany("App\Note")->withTrashed();
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo("App\User")->withTrashed();
     }
 }
