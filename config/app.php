b8cc3497a387708829dc3bd47dadda73e11f9daa
Epsiodes <episodes27@gmail.com>
2017-09-21 13:06:27 +1000

    fixed session timeout issue
    fixed session timeout the user will get TokenMismatchException issue

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         JeroenNoten\LaravelCkEditor\ServiceProvider::class,
+        GeneaLabs\LaravelCaffeine\Providers\LaravelCaffeineService::class,
         
     ],
 
