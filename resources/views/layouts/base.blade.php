60de373eca387e5b6d04258672408fc1bcb30a85
IdrisDose <idrisdev@gmail.com>
2017-09-28 17:19:58 +1000

    Made Jquery-ui local instead of internet grab, hopefully will fix some loadtime issues.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,8 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>NoteBook App</title>
-          <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
     </head>
 
@@ -78,11 +78,11 @@
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
         <script src="https://use.fontawesome.com/c48d3cddbd.js"></script>
         <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
     <!-- <script src="{{ asset('js/jquery3.min.js') }}"></script> -->
         <script src="{{ asset('js/popper.min.js') }}"></script>
+        <script src="{{ asset('js/jquery-ui.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
     </body>
