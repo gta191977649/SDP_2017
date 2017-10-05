7c9f7659c6af39a3ddcea2a0ba5a707d707a5439
Epsiodes <episodes27@gmail.com>
2017-10-06 02:35:42 +1100

    fixed a bug (note body checking)
    fixed a bug the system doesn't check if the user doesn't enter the note body

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -2,8 +2,28 @@
 @section('content')
 
 <div class = "container"> 
+
+    <!-- Error Alert -->
+    <div id="myModal" class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Error</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            The body cannot be empty !
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+        </div>
+        </div>
+    </div>
+    </div>
     <h1>Create Notes</h1>
-    <form action="{{route('notes.store')}}" method="POST">
+    <form action="{{route('notes.store')}}" method="POST" onsubmit="return validateForm()">
     {{ csrf_field() }}
         <div class="form-group">
             <label for"title">Note Title</label>
@@ -12,12 +32,22 @@
         <div class="form-group">
             <label for"body">Note Body</label>
             <!--<input class="form-control" type="text" name="body"> -->
-            <textarea class="form-control" type="text" name="body" rows="10" id="bodyField" required></textarea>
+            <textarea class="form-control" type="text" name="body"  rows="10" id="bodyField" required></textarea>
             @ckeditor('bodyField')
 
         </div>
         <input type="hidden" name="notebook_id" value="{{ $id }}">
-        <input class="btn btn-primary" type="submit" value ="Add">
+        <input class="btn btn-primary" type="submit" value ="Add" >
+        <script>
+            function validateForm()
+            {
+                if (CKEDITOR.instances.bodyField.getData() == '')
+                {
+                    $('#myModal').modal('show')
+                    return false;
+                }
+            }
+        </script>
     </form>
 </div>   
 @endsection
--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -2,8 +2,27 @@
 @section('content')
 
 <div class = "container"> 
+    <!-- Error Alert -->
+    <div id="myModal" class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Error</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            The body cannot be empty !
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+        </div>
+        </div>
+    </div>
+    </div>
     <h1>Edit Note</h1>
-    <form action=" {{ route('notes.update',$note->note_id ) }} " method="POST">
+    <form action=" {{ route('notes.update',$note->note_id ) }} " method="POST" onsubmit="return validateForm()">
     {{ csrf_field() }}
     {{ method_field('PUT')}}
          <div class="form-group">
@@ -13,11 +32,19 @@
         <div class="form-group">
             <label for"body">Note Body</label>
 
-            <textarea class="form-control" type="text" name="body" rows="10" id="bodyField" required>{!!$note->body !!}</textarea>
+            <textarea class="form-control" type="text" name="body" rows="10" id="bodyField"  required>{!!$note->body !!}</textarea>
             @ckeditor('bodyField')
-            
-
         </div>
+        <script>
+            function validateForm()
+            {
+                if (CKEDITOR.instances.bodyField.getData() == '')
+                {
+                    $('#myModal').modal('show')
+                    return false;
+                }
+            }
+        </script>
         <input type="hidden" name="notebook_id" value="{{ $note->notebook_id }}">
         <input class="btn btn-primary" type="submit" value ="Update">
     </form>
