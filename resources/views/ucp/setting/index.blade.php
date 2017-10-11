826e3fe31e14a0a4c01fcf7b08e156949f6a2f11
Epsiodes <episodes27@gmail.com>
2017-10-11 20:53:38 +1100

    fixed the theme display label

--- resources/views/ucp/setting/index.blade.php
+++ resources/views/ucp/setting/index.blade.php
@@ -5,7 +5,15 @@
     <form action="{{ route('ucp.setting.theme')}}" method="post">
         <div class="form-group" >
             {{ csrf_field() }}
-            <label for="theme">Theme: {{ isset(Auth::user()->setting->theme) ? Auth::user()->setting->theme : 0  }}</label>
+            <label for="theme">Theme:
+            @if(isset(Auth::user()->setting->theme))
+                @if(Auth::user()->setting->theme == 0)
+                    Material Design (default)
+                @elseif(Auth::user()->setting->theme == 1)
+                    Dark
+                @endif
+            @endif
+            </label>
             <select class="form-control" id="theme" name="theme">
             <option value="0">Material Design (default)</option>
             <option value="1">Dark</option>
