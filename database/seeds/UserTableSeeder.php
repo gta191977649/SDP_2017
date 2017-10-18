ca151449fcad260ba7c0b67dbcc17c3a381a2edb
IdrisDose <idrisdev@gmail.com>
2017-10-19 04:28:53 +1100

    added changeable variable

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,7 +11,9 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 2)->create()->each(function($u) {
+        $amounToCreate = 2;
+
+        factory(App\User::class, $amounToCreate)->create()->each(function($u) {
             $u->notebooks()->save(factory(App\Notebook::class)->make())->each(function($notebook){
                 $notebook->first()
                     ->notes()->save(factory(App\Note::class)->make())->first()
