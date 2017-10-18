4c8d87e249a6538b486ff65a958582b7e7d7b69d
Epsiodes <episodes27@gmail.com>
2017-10-19 02:16:10 +1100

    fxied the mobile device view for ucp

--- resources/views/layouts/ucp.blade.php
+++ resources/views/layouts/ucp.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>Jouneral App - Dashboard</title>
 
@@ -17,7 +17,7 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <a class="navbar-brand" href="{{ route('ucp.index') }}">Jouenral App Dashboard</a>
       <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
