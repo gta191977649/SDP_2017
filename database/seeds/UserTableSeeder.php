dee3933c02fb9692f407489bb9b439abb74627b6
IdrisDose <idrisdev@gmail.com>
2017-10-19 04:28:20 +1100

    Fixed Seeding

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,9 +13,9 @@ class UserTableSeeder extends Seeder
     {
         factory(App\User::class, 2)->create()->each(function($u) {
             $u->notebooks()->save(factory(App\Notebook::class)->make())->each(function($notebook){
-                $notebook->save(factory(Notebook::class)->make())->first()
-                    ->notes()->save(factory(Note::class)->make())->first()
-                    ->noterecords()->save(factory(NoteRecord::class)->make());
+                $notebook->first()
+                    ->notes()->save(factory(App\Note::class)->make())->first()
+                    ->noterecords()->save(factory(App\NoteRecord::class)->make());
             });
         });
     }
